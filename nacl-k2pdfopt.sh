c6f6816bbc8bb0bccb9ccd455aa1053bba400253
Che-Liang Chiou <clchiou@gmail.com>
2013-04-12 20:30:46 -0700

    Add copyright statement

--- nacl-k2pdfopt.sh
+++ nacl-k2pdfopt.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (c) 2013 Che-Liang Chiou. All rights reserved.
+# Use of this source code is governed by the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
 
 source pkg_info
 source ../common.sh
@@ -17,7 +23,7 @@ CustomPackageInstall() {
   export CC=${NACLCC}
   export CFLAGS="${CFLAGS} -I=/usr/include/glibc-compat"
   export LIBS="-lglibc-compat -lnacl-mounts -lpthread \
-	       -lm -lstdc++ -lcrt_common -lnosys"
+               -lm -lstdc++ -lcrt_common -lnosys"
 
   Banner "Build ${PACKAGE_NAME}"
   echo "Directory: $(pwd)"
